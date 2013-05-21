b5a99da3f7992b79ced8b8832aaf8f74b6f59f22
heysterling <whitebrand>
2013-05-20 21:54:50 -0600

    Created a raisl application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '6040669d24e61d10f27e4be6be2a0d7bc38075411172b64b4233cf10483988367ef356f624851678de6decec0234a92a7e573e3b8d2f0b1e0510da9561d9624e'
