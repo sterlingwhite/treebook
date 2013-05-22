0032234ea6bf2f153f0829632b1ca48a6095ff72
heysterling <whitebrand>
2013-05-21 20:22:27 -0600

    added bootstrap, and applied layout to the page

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Treebook::Application.config.secret_token = '6040669d24e61d10f27e4be6be2a0d7bc38075411172b64b4233cf10483988367ef356f624851678de6decec0234a92a7e573e3b8d2f0b1e0510da9561d9624e'
+Treebook::Application.config.secret_token = '33556d6265e114f2a48ffc4d185dd9e27b33c4a43fb007bf48f0fbb1c7c2bc41b457aa0e95bd82449e5f85b5f6798690acc9c10e5eb512409cbb670d82d47fc5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Treebook::Application.routes.draw do
   resources :statuses
 
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
